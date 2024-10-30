bddf79a6074f29932800d27902227086ccdaa41f
shahriar-hossain <shahriar9412@gmail.com>
2024-10-30 08:20:07 +0600

    router added

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,47 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import AuthProvider from './provider/AuthProvider';
+
+
+import Root from './components/Root';
+import ErrorPage from './components/ErrorPage';
+
+import Home from './components/Home';
+import Login from './components/Login';
+import Register from './components/Register';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>
+      },
+      {
+        path: '/login',
+        element: <Login></Login>
+      },
+      {
+        path: '/register',
+        element: <Register></Register>
+      }
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
+  </React.StrictMode>,
 )
